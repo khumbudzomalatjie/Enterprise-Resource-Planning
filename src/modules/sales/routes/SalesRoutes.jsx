9dd5bb995c09eb4c1a3b343606ad7fc15feb7d23
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-05-27 15:09:02 +0200

    Update SalesRoutes.jsx

--- src/modules/sales/routes/SalesRoutes.jsx
+++ src/modules/sales/routes/SalesRoutes.jsx
@@ -3,21 +3,47 @@ import ProtectedRoute from '../../../components/ProtectedRoute'
 import RoleBasedRoute from '../../../components/RoleBasedRoute'
 import SalesDashboard from '../pages/SalesDashboard'
 import CreateQuotation from '../pages/CreateQuotation'
+import QuotationList from '../pages/QuotationList'
+import QuotationDetail from '../pages/QuotationDetail'
 import { USER_ROLES } from '../../../types/authTypes'
 
 export default function SalesRoutes() {
+  const salesRoles = [USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT, USER_ROLES.FINANCE_OFFICER]
+  const createRoles = [USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]
+
   return (
     <Routes>
       <Route path="/" element={
         <ProtectedRoute>
-          <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT, USER_ROLES.FINANCE_OFFICER]}>
+          <RoleBasedRoute requiredRoles={salesRoles}>
             <SalesDashboard />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+      <Route path="/quotations" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={salesRoles}>
+            <QuotationList />
           </RoleBasedRoute>
         </ProtectedRoute>
       } />
       <Route path="/quotations/new" element={
         <ProtectedRoute>
-          <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]}>
+          <RoleBasedRoute requiredRoles={createRoles}>
+            <CreateQuotation />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+      <Route path="/quotations/:id" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={salesRoles}>
+            <QuotationDetail />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+      <Route path="/quotations/:id/edit" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={createRoles}>
             <CreateQuotation />
           </RoleBasedRoute>
         </ProtectedRoute>
