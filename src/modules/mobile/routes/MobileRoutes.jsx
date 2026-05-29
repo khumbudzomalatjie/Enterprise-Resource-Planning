a9e8a6c8327e95f9f95837fd758d93a32558d54a
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-05-29 12:15:24 +0200

    Update MobileRoutes.jsx

--- src/modules/mobile/routes/MobileRoutes.jsx
+++ src/modules/mobile/routes/MobileRoutes.jsx
@@ -1,18 +1,23 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import ProtectedRoute from '../../../components/ProtectedRoute'
 import MobileHome from '../pages/MobileHome'
+import MyJobs from '../pages/MyJobs'
+import ClockInOut from '../pages/ClockInOut'
+import PhotoUpload from '../pages/PhotoUpload'
+import SuppliesRequest from '../pages/SuppliesRequest'
+import MyProfile from '../pages/MyProfile'
 
 export default function MobileRoutes() {
   return (
     <Routes>
       <Route path="/" element={<ProtectedRoute><MobileHome /></ProtectedRoute>} />
-      <Route path="/jobs" element={<ProtectedRoute><MobileHome /></ProtectedRoute>} />
-      <Route path="/jobs/:id" element={<ProtectedRoute><MobileHome /></ProtectedRoute>} />
-      <Route path="/clock" element={<ProtectedRoute><MobileHome /></ProtectedRoute>} />
-      <Route path="/photos" element={<ProtectedRoute><MobileHome /></ProtectedRoute>} />
-      <Route path="/supplies" element={<ProtectedRoute><MobileHome /></ProtectedRoute>} />
-      <Route path="/incident" element={<ProtectedRoute><MobileHome /></ProtectedRoute>} />
-      <Route path="/profile" element={<ProtectedRoute><MobileHome /></ProtectedRoute>} />
+      <Route path="/jobs" element={<ProtectedRoute><MyJobs /></ProtectedRoute>} />
+      <Route path="/jobs/:id" element={<ProtectedRoute><MyJobs /></ProtectedRoute>} />
+      <Route path="/clock" element={<ProtectedRoute><ClockInOut /></ProtectedRoute>} />
+      <Route path="/photos" element={<ProtectedRoute><PhotoUpload /></ProtectedRoute>} />
+      <Route path="/supplies" element={<ProtectedRoute><SuppliesRequest /></ProtectedRoute>} />
+      <Route path="/incident" element={<ProtectedRoute><SuppliesRequest /></ProtectedRoute>} />
+      <Route path="/profile" element={<ProtectedRoute><MyProfile /></ProtectedRoute>} />
     </Routes>
   )
 }
