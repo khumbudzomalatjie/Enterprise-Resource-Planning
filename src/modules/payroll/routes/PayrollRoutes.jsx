1e1f4e03b927ebbfdbb285c553b395772d74f916
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-06-14 17:25:09 +0200

    Update PayrollRoutes.jsx

--- src/modules/payroll/routes/PayrollRoutes.jsx
+++ src/modules/payroll/routes/PayrollRoutes.jsx
@@ -3,9 +3,13 @@ import ProtectedRoute from '../../../components/ProtectedRoute'
 import RoleBasedRoute from '../../../components/RoleBasedRoute'
 import PayrollDashboard from '../pages/PayrollDashboard'
 import EmployeePayrollList from '../pages/EmployeePayrollList'
+import EmployeePayrollDetail from '../pages/EmployeePayrollDetail'
 import PayslipList from '../pages/PayslipList'
 import PayslipView from '../pages/PayslipView'
 import PayrollRun from '../pages/PayrollRun'
+import PayrollReports from '../pages/PayrollReports'
+import PayrollSettings from '../pages/PayrollSettings'
+import PayrollAudit from '../pages/PayrollAudit'
 import { USER_ROLES } from '../../../types/authTypes'
 
 export default function PayrollRoutes() {
@@ -13,29 +17,16 @@ export default function PayrollRoutes() {
 
   return (
     <Routes>
-      {/* Dashboard */}
       <Route path="/" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><PayrollDashboard /></RoleBasedRoute></ProtectedRoute>} />
-      
-      {/* Employee Payroll Profiles */}
       <Route path="/employees" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><EmployeePayrollList /></RoleBasedRoute></ProtectedRoute>} />
-      <Route path="/employees/:id" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><PayrollDashboard /></RoleBasedRoute></ProtectedRoute>} />
-      
-      {/* Payslips */}
+      <Route path="/employees/:id" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><EmployeePayrollDetail /></RoleBasedRoute></ProtectedRoute>} />
       <Route path="/payslips" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><PayslipList /></RoleBasedRoute></ProtectedRoute>} />
       <Route path="/payslips/:id" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><PayslipView /></RoleBasedRoute></ProtectedRoute>} />
-      
-      {/* Payroll Run */}
       <Route path="/run" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><PayrollRun /></RoleBasedRoute></ProtectedRoute>} />
       <Route path="/run/new" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><PayrollRun /></RoleBasedRoute></ProtectedRoute>} />
-      
-      {/* Reports */}
-      <Route path="/reports" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><PayrollDashboard /></RoleBasedRoute></ProtectedRoute>} />
-      
-      {/* Settings */}
-      <Route path="/settings" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><PayrollDashboard /></RoleBasedRoute></ProtectedRoute>} />
-      
-      {/* Audit */}
-      <Route path="/audit" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><PayrollDashboard /></RoleBasedRoute></ProtectedRoute>} />
+      <Route path="/reports" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><PayrollReports /></RoleBasedRoute></ProtectedRoute>} />
+      <Route path="/settings" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><PayrollSettings /></RoleBasedRoute></ProtectedRoute>} />
+      <Route path="/audit" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><PayrollAudit /></RoleBasedRoute></ProtectedRoute>} />
     </Routes>
   )
 }
