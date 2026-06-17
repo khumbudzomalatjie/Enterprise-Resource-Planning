f94aa2c8cac4bd13dc09974758c7c8551d547b18
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-06-17 12:01:27 +0200

    Update TrackerRoutes.jsx

--- src/modules/tracker/routes/TrackerRoutes.jsx
+++ src/modules/tracker/routes/TrackerRoutes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import ProtectedRoute from '../../../components/ProtectedRoute'
 import TrackerDashboard from '../pages/TrackerDashboard'
+import JobTracker from '../pages/JobTracker'
 
 export default function TrackerRoutes() {
   return (
     <Routes>
       <Route path="/" element={<ProtectedRoute><TrackerDashboard /></ProtectedRoute>} />
+      <Route path="/job" element={<ProtectedRoute><JobTracker /></ProtectedRoute>} />
     </Routes>
   )
 }
