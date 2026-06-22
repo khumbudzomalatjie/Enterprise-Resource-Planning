f04348f36f4dad6154ce912bee729ed8fc468fd2
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-06-22 18:39:06 +0200

    Add LiveJobs route to FieldOpsRoutes

--- src/modules/fieldops/routes/FieldOpsRoutes.jsx
+++ src/modules/fieldops/routes/FieldOpsRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import ProtectedRoute from '../../../components/ProtectedRoute'
 import FieldOpsDashboard from '../pages/FieldOpsDashboard'
 import MessagesDashboard from '../messages/pages/MessagesDashboard'
+import LiveJobs from '../pages/LiveJobs'
 
 export default function FieldOpsRoutes() {
   return (
@@ -9,6 +10,7 @@ export default function FieldOpsRoutes() {
       <Route path="/" element={<ProtectedRoute><FieldOpsDashboard /></ProtectedRoute>} />
       <Route path="/messages" element={<ProtectedRoute><MessagesDashboard /></ProtectedRoute>} />
       <Route path="/messages/:conversationId" element={<ProtectedRoute><MessagesDashboard /></ProtectedRoute>} />
+      <Route path="/jobs" element={<ProtectedRoute><LiveJobs /></ProtectedRoute>} />
     </Routes>
   )
 }
