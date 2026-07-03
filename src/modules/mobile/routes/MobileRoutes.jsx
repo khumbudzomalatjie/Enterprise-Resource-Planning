d56c79748a9242b89be4b62da5cc368fcb683150
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-07-03 13:38:46 +0200

    Update MobileRoutes.jsx

--- src/modules/mobile/routes/MobileRoutes.jsx
+++ src/modules/mobile/routes/MobileRoutes.jsx
@@ -1,23 +1,27 @@
 import { Routes, Route } from 'react-router-dom'
 import ProtectedRoute from '../../../components/ProtectedRoute'
 import MobileDashboard from '../pages/MobileDashboard'
-import MyJobs from '../pages/MyJobs'
 import JobDetail from '../pages/JobDetail'
-import ClockInOut from '../pages/ClockInOut'
-import LeaveManagement from '../pages/LeaveManagement'
-import Messages from '../pages/Messages'
-import Notifications from '../pages/Notifications'
+
+// Lazy load pages that may not exist yet
+const MyJobs = () => {
+  const { default: Component } = await import('../pages/MyJobs')
+  return <Component />
+}
+const WrappedMyJobs = () => {
+  const [Comp, setComp] = useState(null)
+  useEffect(() => { import('../pages/MyJobs').then(m => setComp(() => m.default)) }, [])
+  return Comp ? <Comp /> : <div className="min-h-screen flex items-center justify-center"><div className="animate-spin h-8 w-8 border-2 border-blue-500 border-t-transparent rounded-full"></div></div>
+}
+
+import { useState, useEffect } from 'react'
 
 export default function MobileRoutes() {
   return (
     <Routes>
       <Route path="/" element={<ProtectedRoute><MobileDashboard /></ProtectedRoute>} />
-      <Route path="/jobs" element={<ProtectedRoute><MyJobs /></ProtectedRoute>} />
+      <Route path="/jobs" element={<ProtectedRoute><WrappedMyJobs /></ProtectedRoute>} />
       <Route path="/jobs/:id" element={<ProtectedRoute><JobDetail /></ProtectedRoute>} />
-      <Route path="/attendance" element={<ProtectedRoute><ClockInOut /></ProtectedRoute>} />
-      <Route path="/leave" element={<ProtectedRoute><LeaveManagement /></ProtectedRoute>} />
-      <Route path="/messages" element={<ProtectedRoute><Messages /></ProtectedRoute>} />
-      <Route path="/notifications" element={<ProtectedRoute><Notifications /></ProtectedRoute>} />
     </Routes>
   )
 }
