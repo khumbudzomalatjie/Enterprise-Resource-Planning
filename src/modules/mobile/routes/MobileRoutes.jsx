f28e1557b26affacaf55f38dab35adaf74b1ef11
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-07-04 17:51:27 +0200

    Update MobileRoutes.jsx

--- src/modules/mobile/routes/MobileRoutes.jsx
+++ src/modules/mobile/routes/MobileRoutes.jsx
@@ -3,6 +3,9 @@ import ProtectedRoute from '../../../components/ProtectedRoute'
 import MobileDashboard from '../pages/MobileDashboard'
 import MyJobs from '../pages/MyJobs'
 import JobDetail from '../pages/JobDetail'
+import Messages from '../pages/Messages'
+import LeaveManagement from '../pages/LeaveManagement'
+import Profile from '../pages/Profile'
 
 export default function MobileRoutes() {
   return (
@@ -10,6 +13,9 @@ export default function MobileRoutes() {
       <Route path="/" element={<ProtectedRoute><MobileDashboard /></ProtectedRoute>} />
       <Route path="/jobs" element={<ProtectedRoute><MyJobs /></ProtectedRoute>} />
       <Route path="/jobs/:id" element={<ProtectedRoute><JobDetail /></ProtectedRoute>} />
+      <Route path="/messages" element={<ProtectedRoute><Messages /></ProtectedRoute>} />
+      <Route path="/leave" element={<ProtectedRoute><LeaveManagement /></ProtectedRoute>} />
+      <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
       <Route path="*" element={<ProtectedRoute><MobileDashboard /></ProtectedRoute>} />
     </Routes>
   )
