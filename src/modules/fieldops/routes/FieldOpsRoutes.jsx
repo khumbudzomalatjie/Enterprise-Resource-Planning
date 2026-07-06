eefc9c2d02b39c34e1132a377732599bc7e45cad
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-07-06 19:43:48 +0200

    Update FieldOpsRoutes.jsx

--- src/modules/fieldops/routes/FieldOpsRoutes.jsx
+++ src/modules/fieldops/routes/FieldOpsRoutes.jsx
@@ -1,20 +1,39 @@
 import { Routes, Route } from 'react-router-dom'
 import ProtectedRoute from '../../../components/ProtectedRoute'
+import RoleBasedRoute from '../../../components/RoleBasedRoute'
 import FieldOpsDashboard from '../pages/FieldOpsDashboard'
 import LiveJobs from '../pages/LiveJobs'
-import Incidents from '../pages/Incidents'
-import ReportIncident from '../pages/ReportIncident'
 import JobTracker from '../pages/JobTracker'
 
+// NEW Enterprise Incident Management
+import IncidentDashboard from '../incidents/pages/IncidentDashboard'
+import ReportIncident from '../incidents/pages/ReportIncident'
+import IncidentList from '../incidents/pages/IncidentList'
+import IncidentDetail from '../incidents/pages/IncidentDetail'
+
+import { USER_ROLES } from '../../../types/authTypes'
+
 export default function FieldOpsRoutes() {
+  const allRoles = [USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SUPERVISOR, USER_ROLES.HR_MANAGER, USER_ROLES.FINANCE_OFFICER, USER_ROLES.SALES_AGENT, USER_ROLES.CLEANER]
+
   return (
     <Routes>
-      <Route path="/" element={<ProtectedRoute><FieldOpsDashboard /></ProtectedRoute>} />
-      <Route path="/live-jobs" element={<ProtectedRoute><LiveJobs /></ProtectedRoute>} />
-      <Route path="/incidents" element={<ProtectedRoute><Incidents /></ProtectedRoute>} />
-      <Route path="/incidents/new" element={<ProtectedRoute><ReportIncident /></ProtectedRoute>} />
-      <Route path="/incidents/:id" element={<ProtectedRoute><ReportIncident /></ProtectedRoute>} />
-      <Route path="/job-tracker" element={<ProtectedRoute><JobTracker /></ProtectedRoute>} />
+      {/* Dashboard */}
+      <Route path="/" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allRoles}><FieldOpsDashboard /></RoleBasedRoute></ProtectedRoute>} />
+      
+      {/* Live Jobs */}
+      <Route path="/live-jobs" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allRoles}><LiveJobs /></RoleBasedRoute></ProtectedRoute>} />
+      
+      {/* Job Tracker */}
+      <Route path="/job-tracker" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allRoles}><JobTracker /></RoleBasedRoute></ProtectedRoute>} />
+
+      {/* ============================================ */}
+      {/* NEW ENTERPRISE INCIDENT MANAGEMENT */}
+      {/* ============================================ */}
+      <Route path="/incidents" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allRoles}><IncidentDashboard /></RoleBasedRoute></ProtectedRoute>} />
+      <Route path="/incidents/report" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allRoles}><ReportIncident /></RoleBasedRoute></ProtectedRoute>} />
+      <Route path="/incidents/list" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allRoles}><IncidentList /></RoleBasedRoute></ProtectedRoute>} />
+      <Route path="/incidents/:id" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allRoles}><IncidentDetail /></RoleBasedRoute></ProtectedRoute>} />
     </Routes>
   )
 }
