8791d284f04cfa692f9d7529ca07c0a728daccae
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-07-09 10:24:37 +0200

    Update InventoryRoutes.jsx

--- src/modules/inventory/routes/InventoryRoutes.jsx
+++ src/modules/inventory/routes/InventoryRoutes.jsx
@@ -3,9 +3,9 @@ import ProtectedRoute from '../../../components/ProtectedRoute'
 import RoleBasedRoute from '../../../components/RoleBasedRoute'
 import InventoryDashboard from '../pages/InventoryDashboard'
 import StockList from '../pages/StockList'
-import AddItem from '../pages/AddItem'
-import StockIn from '../pages/StockIn'
-import StockOut from '../pages/StockOut'
+import ServiceList from '../pages/ServiceList'
+import ServiceForm from '../pages/ServiceForm'
+import ProductList from '../pages/ProductList'
 import { USER_ROLES } from '../../../types/authTypes'
 
 export default function InventoryRoutes() {
@@ -14,58 +14,19 @@ export default function InventoryRoutes() {
   return (
     <Routes>
       {/* Dashboard */}
-      <Route path="/" element={
-        <ProtectedRoute>
-          <RoleBasedRoute requiredRoles={allowedRoles}>
-            <InventoryDashboard />
-          </RoleBasedRoute>
-        </ProtectedRoute>
-      } />
+      <Route path="/" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><InventoryDashboard /></RoleBasedRoute></ProtectedRoute>} />
       
-      {/* Stock List */}
-      <Route path="/items" element={
-        <ProtectedRoute>
-          <RoleBasedRoute requiredRoles={allowedRoles}>
-            <StockList />
-          </RoleBasedRoute>
-        </ProtectedRoute>
-      } />
-      
-      {/* Add New Item */}
-      <Route path="/items/new" element={
-        <ProtectedRoute>
-          <RoleBasedRoute requiredRoles={allowedRoles}>
-            <AddItem />
-          </RoleBasedRoute>
-        </ProtectedRoute>
-      } />
-      
-      {/* Edit Item */}
-      <Route path="/items/:id" element={
-        <ProtectedRoute>
-          <RoleBasedRoute requiredRoles={allowedRoles}>
-            <AddItem />
-          </RoleBasedRoute>
-        </ProtectedRoute>
-      } />
-      
-      {/* Stock In */}
-      <Route path="/stock-in" element={
-        <ProtectedRoute>
-          <RoleBasedRoute requiredRoles={allowedRoles}>
-            <StockIn />
-          </RoleBasedRoute>
-        </ProtectedRoute>
-      } />
-      
-      {/* Stock Out */}
-      <Route path="/stock-out" element={
-        <ProtectedRoute>
-          <RoleBasedRoute requiredRoles={allowedRoles}>
-            <StockOut />
-          </RoleBasedRoute>
-        </ProtectedRoute>
-      } />
+      {/* Stock/Items */}
+      <Route path="/items" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><StockList /></RoleBasedRoute></ProtectedRoute>} />
+      <Route path="/items/:id" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><StockList /></RoleBasedRoute></ProtectedRoute>} />
+
+      {/* Services & Pricing */}
+      <Route path="/services" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><ServiceList /></RoleBasedRoute></ProtectedRoute>} />
+      <Route path="/services/new" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><ServiceForm /></RoleBasedRoute></ProtectedRoute>} />
+      <Route path="/services/:id/edit" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><ServiceForm /></RoleBasedRoute></ProtectedRoute>} />
+
+      {/* Consumable Products */}
+      <Route path="/products" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><ProductList /></RoleBasedRoute></ProtectedRoute>} />
     </Routes>
   )
 }
