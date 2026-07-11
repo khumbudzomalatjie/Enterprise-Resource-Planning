047044bb1acb852ec1675cb20e8cabc7d23e43b1
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-07-11 17:33:48 +0200

    Update InventoryRoutes.jsx

--- src/modules/inventory/routes/InventoryRoutes.jsx
+++ src/modules/inventory/routes/InventoryRoutes.jsx
@@ -6,6 +6,7 @@ import StockList from '../pages/StockList'
 import ServiceList from '../pages/ServiceList'
 import ServiceForm from '../pages/ServiceForm'
 import ProductList from '../pages/ProductList'
+import ConsumableProducts from '../pages/ConsumableProducts'
 import { USER_ROLES } from '../../../types/authTypes'
 
 export default function InventoryRoutes() {
@@ -13,20 +14,81 @@ export default function InventoryRoutes() {
 
   return (
     <Routes>
-      {/* Dashboard */}
-      <Route path="/" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><InventoryDashboard /></RoleBasedRoute></ProtectedRoute>} />
+      {/* ============================================ */}
+      {/* INVENTORY DASHBOARD                          */}
+      {/* ============================================ */}
+      <Route path="/" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={allowedRoles}>
+            <InventoryDashboard />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
       
-      {/* Stock/Items */}
-      <Route path="/items" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><StockList /></RoleBasedRoute></ProtectedRoute>} />
-      <Route path="/items/:id" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><StockList /></RoleBasedRoute></ProtectedRoute>} />
+      {/* ============================================ */}
+      {/* STOCK / ITEMS                                */}
+      {/* ============================================ */}
+      <Route path="/items" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={allowedRoles}>
+            <StockList />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+      <Route path="/items/:id" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={allowedRoles}>
+            <StockList />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
 
-      {/* Services & Pricing */}
-      <Route path="/services" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><ServiceList /></RoleBasedRoute></ProtectedRoute>} />
-      <Route path="/services/new" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><ServiceForm /></RoleBasedRoute></ProtectedRoute>} />
-      <Route path="/services/:id/edit" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><ServiceForm /></RoleBasedRoute></ProtectedRoute>} />
+      {/* ============================================ */}
+      {/* SERVICES & PRICING                           */}
+      {/* ============================================ */}
+      <Route path="/services" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={allowedRoles}>
+            <ServiceList />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+      <Route path="/services/new" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={allowedRoles}>
+            <ServiceForm />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+      <Route path="/services/:id/edit" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={allowedRoles}>
+            <ServiceForm />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
 
-      {/* Consumable Products */}
-      <Route path="/products" element={<ProtectedRoute><RoleBasedRoute requiredRoles={allowedRoles}><ProductList /></RoleBasedRoute></ProtectedRoute>} />
+      {/* ============================================ */}
+      {/* CONSUMABLE PRODUCTS                          */}
+      {/* ============================================ */}
+      <Route path="/consumables" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={allowedRoles}>
+            <ConsumableProducts />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+
+      {/* ============================================ */}
+      {/* PRODUCTS (GENERAL)                           */}
+      {/* ============================================ */}
+      <Route path="/products" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={allowedRoles}>
+            <ProductList />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
     </Routes>
   )
 }
