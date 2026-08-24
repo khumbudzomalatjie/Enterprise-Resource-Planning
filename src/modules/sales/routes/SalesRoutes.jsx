c6f645beebe8a4d462f081e7a0ba3b098fcb1182
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-08-24 20:38:24 +0200

    Update SalesRoutes.jsx

--- src/modules/sales/routes/SalesRoutes.jsx
+++ src/modules/sales/routes/SalesRoutes.jsx
@@ -5,6 +5,9 @@ import SalesDashboard from '../pages/SalesDashboard'
 import CreateQuotation from '../pages/CreateQuotation'
 import QuotationList from '../pages/QuotationList'
 import QuotationDetail from '../pages/QuotationDetail'
+import InvoiceList from '../pages/InvoiceList'
+import SalesReports from '../pages/SalesReports'
+import PaymentList from '../pages/PaymentList'
 import { USER_ROLES } from '../../../types/authTypes'
 
 export default function SalesRoutes() {
@@ -13,7 +16,9 @@ export default function SalesRoutes() {
 
   return (
     <Routes>
-      {/* Sales Dashboard */}
+      {/* ============================================ */}
+      {/* SALES DASHBOARD                              */}
+      {/* ============================================ */}
       <Route path="/" element={
         <ProtectedRoute>
           <RoleBasedRoute requiredRoles={salesRoles}>
@@ -21,6 +26,10 @@ export default function SalesRoutes() {
           </RoleBasedRoute>
         </ProtectedRoute>
       } />
+      
+      {/* ============================================ */}
+      {/* QUOTATIONS                                   */}
+      {/* ============================================ */}
       
       {/* Quotation List */}
       <Route path="/quotations" element={
@@ -54,6 +63,39 @@ export default function SalesRoutes() {
         <ProtectedRoute>
           <RoleBasedRoute requiredRoles={salesRoles}>
             <QuotationDetail />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+
+      {/* ============================================ */}
+      {/* INVOICES                                     */}
+      {/* ============================================ */}
+      <Route path="/invoices" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={salesRoles}>
+            <InvoiceList />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+
+      {/* ============================================ */}
+      {/* SALES REPORTS                                */}
+      {/* ============================================ */}
+      <Route path="/reports" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={salesRoles}>
+            <SalesReports />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+
+      {/* ============================================ */}
+      {/* PAYMENTS                                     */}
+      {/* ============================================ */}
+      <Route path="/payments" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={salesRoles}>
+            <PaymentList />
           </RoleBasedRoute>
         </ProtectedRoute>
       } />
