3b281a4dd7adc99d88c582a235b950c5973a26b1
khumbudzomalatjie <khumbudzomalatjie@gmail.com>
2026-09-11 17:58:50 +0200

    Update CRMRoutes.jsx

--- src/modules/crm/routes/CRMRoutes.jsx
+++ src/modules/crm/routes/CRMRoutes.jsx
@@ -4,15 +4,20 @@ import RoleBasedRoute from '../../../components/RoleBasedRoute'
 import CRMDashboard from '../pages/CRMDashboard'
 import ClientList from '../pages/ClientList'
 import ClientDetail from '../pages/ClientDetail'
+import ContactList from '../pages/ContactList'
+import PipelineBoard from '../pages/PipelineBoard'
+import ServiceList from '../pages/ServiceList'
 import { USER_ROLES } from '../../../types/authTypes'
 
 export default function CRMRoutes() {
+  const allowedRoles = [USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]
+
   return (
     <Routes>
       {/* Dashboard */}
       <Route path="/" element={
         <ProtectedRoute>
-          <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]}>
+          <RoleBasedRoute requiredRoles={allowedRoles}>
             <CRMDashboard />
           </RoleBasedRoute>
         </ProtectedRoute>
@@ -21,17 +26,44 @@ export default function CRMRoutes() {
       {/* Client List - MUST come before /:id */}
       <Route path="/clients" element={
         <ProtectedRoute>
-          <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]}>
+          <RoleBasedRoute requiredRoles={allowedRoles}>
             <ClientList />
           </RoleBasedRoute>
         </ProtectedRoute>
       } />
       
-      {/* Client Detail - Only match UUID patterns */}
+      {/* Client Detail */}
       <Route path="/clients/:id" element={
         <ProtectedRoute>
-          <RoleBasedRoute requiredRoles={[USER_ROLES.SUPER_ADMIN, USER_ROLES.OPERATIONS_MANAGER, USER_ROLES.SALES_AGENT]}>
+          <RoleBasedRoute requiredRoles={allowedRoles}>
             <ClientDetail />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+
+      {/* ✅ NEW: Contacts */}
+      <Route path="/contacts" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={allowedRoles}>
+            <ContactList />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+
+      {/* ✅ NEW: Pipeline */}
+      <Route path="/pipeline" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={allowedRoles}>
+            <PipelineBoard />
+          </RoleBasedRoute>
+        </ProtectedRoute>
+      } />
+
+      {/* ✅ NEW: Services */}
+      <Route path="/services" element={
+        <ProtectedRoute>
+          <RoleBasedRoute requiredRoles={allowedRoles}>
+            <ServiceList />
           </RoleBasedRoute>
         </ProtectedRoute>
       } />
